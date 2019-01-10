603b4c5fdf2e4d6c7f9e760aa85219ebafa69205
SalwaAhmedfci <37433010+SalwaAhmedfci@users.noreply.github.com>
2019-01-10 07:42:22 -0500

    Reformatting

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-#bigRating strong margRtSm h1
+# bigRating strong margRtSm h1
 
 
-#!/usr/bin/env python
-import time
+# !/usr/bin/env python
 import requests
-from bs4 import  BeautifulSoup
-from pandas import DataFrame
-from urllib.request import urlopen
+from bs4 import BeautifulSoup
 from pip._vendor.distlib.compat import raw_input
-from requests import get
-from userAgent import user_agents, randomUserAgents
-import lxml
 
-
+from userAgent import randomUserAgents
 
 companyname = raw_input("enter company name: ")
 head = randomUserAgents()
-start = time.time()
 
-page_link ="https://www.glassdoor.com/Reviews/company-reviews.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword='{0}'&sc.keyword='{1}'&locT=&locId=&jobType=".format(companyname,companyname)
+page_link = "https://www.glassdoor.com/Reviews/company-reviews.htm?suggestCount=0&suggestChosen=false&clickSource" \
+            "=searchBtn&typedKeyword='{0}'&sc.keyword='{1}'&locT=&locId=&jobType=".format(
+        companyname, companyname)
 
 
-
-def soup(url,headers):
+def soup(url, headers):
     session = requests.Session()
     req = session.get(url, headers=headers)
     bs = BeautifulSoup(req.text, 'lxml')
