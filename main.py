3b02f0413eddfd1f60d30b34479acf77f9eb2d73
SalwaAhmedfci <37433010+SalwaAhmedfci@users.noreply.github.com>
2019-01-10 06:18:49 -0500

    Fix headers error

--- main.py
+++ main.py
@@ -8,25 +8,28 @@ from bs4 import  BeautifulSoup
 from pandas import DataFrame
 from urllib.request import urlopen
 from pip._vendor.distlib.compat import raw_input
-
+from requests import get
 from userAgent import user_agents, randomUserAgents
 import lxml
 
 
 
 companyname = raw_input("enter company name: ")
-
+head = randomUserAgents()
+start = time.time()
 
 page_link ="https://www.glassdoor.com/Reviews/company-reviews.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword='{0}'&sc.keyword='{1}'&locT=&locId=&jobType=".format(companyname,companyname)
-head = randomUserAgents()
 
 
-r = requests.get(page_link,head)
 
-data = r.text
+def soup(url,headers):
+    session = requests.Session()
+    req = session.get(url, headers=headers)
+    bs = BeautifulSoup(req.text, 'lxml')
+    return bs
 
-soup = BeautifulSoup(data,'lxml')
-print(soup)
-for rate in soup.find_all('span', class_='bigRating strong margRtSm h1'):
 
-    print(rate)
+bs = soup(page_link, head)
+rate = bs.find('span', {'class', "bigRating strong margRtSm h1"}).get_text()
+
+print(rate)
